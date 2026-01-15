616b0fcc7be95edc60adc5eaeedba8194b2201f3
KaneLogger <741180788@qq.com>
2026-01-15 09:59:44 +0800

    chore: 隐藏复杂提示词

--- demo/frontend/src/App.vue
+++ demo/frontend/src/App.vue
@@ -12,7 +12,7 @@
           <el-menu-item index="/">首页</el-menu-item>
           <el-menu-item index="/minimal">最小公式</el-menu-item>
           <el-menu-item index="/templates">提示词模版</el-menu-item>
-          <el-menu-item index="/complex">复杂提示词</el-menu-item>
+          <!-- <el-menu-item index="/complex">复杂提示词</el-menu-item> -->
         </el-menu>
         <div class="header-right">
           <a href="https://github.com/hk4114/prompt-strategy" target="_blank" class="github-link">
